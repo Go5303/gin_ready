734dce31a852f6d80569b24b4a5087f7d94f8cbe
张海 <zhanghai@woda.ink>
2024-07-08 12:22:33 +0800

    -fix

--- main.go
+++ main.go
@@ -6,23 +6,29 @@ import (
 	"gin_ready/global"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
+	"gorm.io/gorm"
 	"net/http"
 )
 
 var (
 	Log  *zap.Logger
 	Yaml config.Configuration
+	DB   *gorm.DB
 )
 
 func main() {
-	//初始化环境配置
+	//初始化yaml配置
 	bootstrap.InitializeConfig()
 	Yaml = global.App.Config
 
-	//初始化日志
+	//初始化log
 	global.App.Log = bootstrap.InitializeLog()
 	Log = global.App.Log
 	Log.Info("log init success!")
+
+	//数据库初始化
+	DB = bootstrap.InitializeDB()
+	Log.Info("db init success")
 
 	r := gin.Default()
 
